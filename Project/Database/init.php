f929d36f943f048a2bd43a127cabd70a5ca67d90
101227936 <101227936@students.swinburne.edu.my>
2020-10-04 15:59:06 +0800

    Update init.php

--- Project/Database/init.php
+++ Project/Database/init.php
@@ -1,8 +1,9 @@
 <?php
 	require_once 'MysqliDb.php';
-	$db = new MysqliDb('localhost', 'howtoing', 'howtoing', 'howtoing' );
-	$db->where("id>1")->where("email='chuadevice@gmail.com'");
-	$users = $db->get('tbluser');
+	$db = new MysqliDb('localhost', 'howtoing', 'howtoing', 'howtoing' );//server
+	//$db = new MysqliDb('localhost', 'root', '', 'howtoing' );//loachost
+	$db->where("id>1")->where("email='fcmsmember@gmail.com'");
+	$users = $db->get('tbl_login');
 	print_r ("<pre>");
 	print_r($users);
 	print_r ("</pre>");
