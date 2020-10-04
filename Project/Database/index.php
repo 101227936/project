dfbe490aa216c1895e7bea8e340ebeec481949f4
101227936 <101227936@students.swinburne.edu.my>
2020-10-04 15:59:58 +0800

    fix bug

--- Project/Database/index.php
+++ Project/Database/index.php
@@ -1,6 +1,6 @@
 <?php
 	require_once 'init.php';
-	$db->where("id>1")->where("email='chuadevice@gmail.com'");
+	$db->where("id>1")->where("email='fcmsmember@gmail.com'");
 	$users = $db->get('tbluser');
 	print_r ("<pre>");
 	print_r($users);
--- Project/Database/init.php
+++ Project/Database/init.php
@@ -2,9 +2,4 @@
 	require_once 'MysqliDb.php';
 	$db = new MysqliDb('localhost', 'howtoing', 'howtoing', 'howtoing' );//server
 	//$db = new MysqliDb('localhost', 'root', '', 'howtoing' );//loachost
-	$db->where("id>1")->where("email='fcmsmember@gmail.com'");
-	$users = $db->get('tbl_login');
-	print_r ("<pre>");
-	print_r($users);
-	print_r ("</pre>");
 ?>
