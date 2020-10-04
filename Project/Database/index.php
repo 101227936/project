514f85e16e44654ccfeb121caa8c8027f3d2838a
101227936 <101227936@students.swinburne.edu.my>
2020-10-04 16:00:36 +0800

    Update index.php

--- Project/Database/index.php
+++ Project/Database/index.php
@@ -1,6 +1,6 @@
 <?php
 	require_once 'init.php';
-	$db->where("id>1")->where("email='fcmsmember@gmail.com'");
+	$db->where("login_id>1")->where("email='fcmsmember@gmail.com'");
 	$users = $db->get('tbluser');
 	print_r ("<pre>");
 	print_r($users);
