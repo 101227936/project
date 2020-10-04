900ee6d598b0450fa123230ab641b4a02a747db6
101227936 <101227936@students.swinburne.edu.my>
2020-10-04 17:28:35 +0800

    Update index.php

--- Project/Database/index.php
+++ Project/Database/index.php
@@ -4,7 +4,7 @@
 	$db->where("email='fcmsmember@gmail.com'");
 	$users = $db->get('tbl_login');
 	print_r ("<pre>");
-	print_r($users);
+	print_r($users[0]["login_id"]);
 	print_r ("</pre>");
 ?>
 <html>
