1638f4d3c3597717ce7ab467b40dac42e5bc59b0
elaine_0207 <elaine_0207@yahoo.com>
2020-10-04 21:28:49 +0800

    E-commerce OrderList and php

--- Project/Database/init.php
+++ Project/Database/init.php
@@ -1,5 +1,5 @@
 <?php
 	require_once 'MysqliDb.php';
-	$db = new MysqliDb('localhost', 'howtoing', 'howtoing', 'howtoing' );//server
-	//$db = new MysqliDb('localhost', 'root', '', 'howtoing' );//loachost
+	//$db = new MysqliDb('localhost', 'howtoing', 'howtoing', 'howtoing' );//server
+	$db = new MysqliDb('localhost', 'root', '', 'howtoing' );//loachost
 ?>
