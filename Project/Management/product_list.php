d08d86bb704327a7da46b283d992edb2ebca10ca
Harold <55337527+Juicearian@users.noreply.github.com>
2020-10-11 11:16:52 +0800

    Update product_list.php

--- Project/Management/product_list.php
+++ Project/Management/product_list.php
@@ -58,7 +58,7 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="page-title-box">
-                                    <h4 class="page-title">Product</h4>
+                                    <h4 class="page-title">Product List</h4>
                                 </div>
                             </div>
                         </div>     
