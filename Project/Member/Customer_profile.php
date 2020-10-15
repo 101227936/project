d59418198338dce4fb57ae62a2fd8d2819d3708b
Harold <55337527+Juicearian@users.noreply.github.com>
2020-10-15 10:06:20 +0800

    change the edit password method

--- Project/Member/Customer_profile.php
+++ Project/Member/Customer_profile.php
@@ -1,8 +1,10 @@
 <?php
 require "../Database/init.php";
 require "../encrypt.php";
-
+ob_start();
 error_reporting(0);
+$_SESSION['user_id']=1;
+if(empty($_SESSION['user_id']))header("Location: ../Landing/landing.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,65 +34,130 @@ error_reporting(0);
             if(isset($_POST['btnSave']))
             {
                 $db->join("tbl_login l", "l.login_id=u.login_id", "INNER");
-                $db->where("u.login_id", 3);
+                $db->where("u.user_id",$_SESSION['user_id'] ,"=");
                 $rows = $db->getOne ("tbl_user u");
 
                 if(isset($_FILES['image']))
                 {
                     $old_image = $rows['user_profile'];
+                    $random_Name = date("YmdHis");
                     $error = 0;
-                    $file_name = "../Image/Profile/".$_FILES['image']['name'];
+                    $file_name = "../Image/Profile/".$random_Name.'.'.explode("/",$_FILES['image']['type'])[1];
                     $file_size =$_FILES['image']['size'];
                     $file_tmp =$_FILES['image']['tmp_name'];
                     $file_type=$_FILES['image']['type'];
                    
-                    /*if (file_exists($file_name)) {
-                        echo "<script> alert('Sorry, file already exists.');location='Customer_profile.php'</script>";
-                        $error = 1;
-                    }*/
-
                     if($file_size > 2097152){
                         echo "<script> alert('File size must be excately 2 MB');location='Customer_profile.php'</script>";
                         $error = 1;
                     }
                     if($error==0)
                     {
-                        if ($file_size == 0){
-                            $test = encrypt_decrypt("encrypt",trim($_POST['password']));
-                            $data = Array (
-                                'user_name' =>trim($_POST['name']),
-                                'user_phone' => trim($_POST['userPhone']),
-                                'user_address' => trim($_POST['userAddress']),
-                                'email' => trim($_POST['userEmail']),
-                                'password' => $test,
-                                'user_profile' => $old_image
-                            );
-                            $db->join("tbl_login l", "l.login_id=u.login_id", "INNER");
-                            $db->where ('u.login_id', 3);
-                            if ($db->update ('tbl_user u', $data))
-                                echo "<script> alert('Save change');location='Customer_profile.php'</script>";
+                        if ($file_size == 0)
+                        {
+                            if(trim($_POST['password'])=="")
+                            {
+                               $old_password = $rows['password'];
+                               //$test = encrypt_decrypt("encrypt",trim($_POST['password']));
+                               $data = Array (
+                                   'user_name' =>trim($_POST['name']),
+                                   'user_phone' => trim($_POST['userPhone']),
+                                   'user_address' => trim($_POST['userAddress']),
+                                   'email' => trim($_POST['userEmail']),
+                                   'password' => $old_password,
+                                   'user_profile' => $old_image
+                               );
+                               $db->join("tbl_login l", "l.login_id=u.login_id", "INNER");
+                               $db->where ('u.login_id', 3);
+                               if ($db->update ('tbl_user u', $data))
+                                   echo "<script> alert('Save change');location='Customer_profile.php'</script>";
+                               else
+                                   echo 'update failed: ' . $db->getLastError();
+                            }
+                            else if($_POST['password']==encrypt_decrypt("decrypt",trim($rows['password'])))
+                            {
+                                if(trim($_POST['New_password'])=="")
+                                {
+                                    echo "<script> alert('New password cannot be empty or space.');location='Customer_profile.php'</script>";
+                                }
+                                else
+                                {
+                                    $new_password = encrypt_decrypt("encrypt",trim($_POST['New_password']));
+                                    $data = Array (
+                                        'user_name' =>trim($_POST['name']),
+                                        'user_phone' => trim($_POST['userPhone']),
+                                        'user_address' => trim($_POST['userAddress']),
+                                        'email' => trim($_POST['userEmail']),
+                                        'password' => $new_password,
+                                        'user_profile' => $old_image
+                                    );
+                                    $db->join("tbl_login l", "l.login_id=u.login_id", "INNER");
+                                    $db->where ('u.login_id', 3);
+                                    if ($db->update ('tbl_user u', $data))
+                                        echo "<script> alert('Save change');location='Customer_profile.php'</script>";
+                                    else
+                                        echo 'update failed: ' . $db->getLastError();
+                                }
+                            }
                             else
-                                echo 'update failed: ' . $db->getLastError();
+                            {
+                                echo "<script> alert('Your Current Password is wrong.');location='Customer_profile.php'</script>";
+                            }
                         }
                         else
                         {
-                            unlink($old_image);
-                            move_uploaded_file($file_tmp,$file_name);
-                            $test = encrypt_decrypt("encrypt",trim($_POST['password']));
-                            $data = Array (
-                                'user_name' =>trim($_POST['name']),
-                                'user_phone' => trim($_POST['userPhone']),
-                                'user_address' => trim($_POST['userAddress']),
-                                'email' => trim($_POST['userEmail']),
-                                'password' => $test,
-                                'user_profile' => $file_name
-                            );
-                            $db->join("tbl_login l", "l.login_id=u.login_id", "INNER");
-                            $db->where ('u.login_id', 3);
-                            if ($db->update ('tbl_user u', $data))
-                                echo "<script> alert('Save change');location='Customer_profile.php'</script>";
+                            if(trim($_POST['password'])=="")
+                            {
+                                unlink($old_image);
+                                move_uploaded_file($file_tmp,$file_name);
+                                $old_password = $rows['password'];
+                               //$test = encrypt_decrypt("encrypt",trim($_POST['password']));
+                               $data = Array (
+                                   'user_name' =>trim($_POST['name']),
+                                   'user_phone' => trim($_POST['userPhone']),
+                                   'user_address' => trim($_POST['userAddress']),
+                                   'email' => trim($_POST['userEmail']),
+                                   'password' => $old_password,
+                                   'user_profile' => $file_name
+                               );
+                               $db->join("tbl_login l", "l.login_id=u.login_id", "INNER");
+                               $db->where ('u.login_id', 3);
+                               if ($db->update ('tbl_user u', $data))
+                                   echo "<script> alert('Save change');location='Customer_profile.php'</script>";
+                               else
+                                   echo 'update failed: ' . $db->getLastError();
+                            }
+                            else if($_POST['password']==encrypt_decrypt("decrypt",trim($rows['password'])))
+                            {
+                                if(trim($_POST['New_password'])=="")
+                                {
+                                    echo "<script> alert('New password cannot be empty or space.');location='Customer_profile.php'</script>";
+                                }
+                                else
+                                {
+                                    unlink($old_image);
+                                    move_uploaded_file($file_tmp,$file_name);
+                                    $new_password = encrypt_decrypt("encrypt",trim($_POST['New_password']));
+                                    $data = Array (
+                                        'user_name' =>trim($_POST['name']),
+                                        'user_phone' => trim($_POST['userPhone']),
+                                        'user_address' => trim($_POST['userAddress']),
+                                        'email' => trim($_POST['userEmail']),
+                                        'password' => $new_password,
+                                        'user_profile' => $file_name
+                                    );
+                                    $db->join("tbl_login l", "l.login_id=u.login_id", "INNER");
+                                    $db->where ('u.login_id', 3);
+                                    if ($db->update ('tbl_user u', $data))
+                                        echo "<script> alert('Save change');location='Customer_profile.php'</script>";
+                                    else
+                                        echo 'update failed: ' . $db->getLastError();
+                                }
+                            }
                             else
-                                echo 'update failed: ' . $db->getLastError();
+                            {
+                                echo "<script> alert('Your Current Password is wrong.');location='Customer_profile.php'</script>";
+                            }
                         }
                     }
                     else
@@ -164,35 +231,18 @@ error_reporting(0);
                                         <div class="tab-pane show active" id="settings">
                                             <form method="post" action="" id="userForm" class="parsley-examples" enctype="multipart/form-data">
                                                 <div class="row">
-                                                    <div class="col-12">
+                                                    <div class="col-md-6">
                                                         <div class="form-group">
                                                             <label for="image">Upload Your Photo(Less than 2MB)</label>
                                                             <input type="file" name="image" id="image" class="form-control-file" accept="image/*">
                                                         </div>
                                                     </div> <!-- end col -->
-                                                </div> <!-- end row -->
-                                                <div class="row">
                                                     <div class="col-md-6">
                                                         <div class="form-group">
                                                             <label for="name">Your Name<span class="text-danger">*</span></label>
                                                             <input type="text" name="name" parsley-trigger="change" required placeholder="Enter your name" class="form-control" id="name" value="<?php echo $rows['user_name']?>">
                                                         </div>
                                                     </div>
-                                                    <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <?php $output = encrypt_decrypt("decrypt",trim($rows['password']));?>
-                                                            <label for="password">Password<span class="text-danger">*</span></label>
-                                                            <input type="password" name="password" parsley-trigger="change" required placeholder="Enter new password" class="form-control" id="password" value="<?php echo $output?>">
-                                                        </div>
-                                                    </div>
-                                                </div> <!-- end row -->
-                                                <div class="row">
-                                                    <div class="col-12">
-                                                        <div class="form-group">
-                                                            <label for="userAddress">Address<span class="text-danger">*</span></label>
-                                                            <textarea class="form-control" id="userAddress" name="userAddress" rows="4" parsley-trigger="change" required placeholder="Enter your new address..."><?php echo $rows['user_address']?></textarea>
-                                                        </div>
-                                                    </div> <!-- end col -->
                                                 </div> <!-- end row -->
                                                 <div class="row">
                                                     <div class="col-md-6">
@@ -207,6 +257,29 @@ error_reporting(0);
                                                             <input type="type" name="userPhone" data-parsley-type="digits" data-parsley-length="[10,11]" parsley-trigger="change" required placeholder="Enter new phone number" class="form-control" id="userPhone" value="<?php echo $rows['user_phone']?>">
                                                         </div>
                                                     </div> <!-- end col -->
+                                                </div> <!-- end row -->
+                                                <div class="row">
+                                                    <div class="col-12">
+                                                        <div class="form-group">
+                                                            <label for="userAddress">Address<span class="text-danger">*</span></label>
+                                                            <textarea class="form-control" id="userAddress" name="userAddress" rows="2" parsley-trigger="change" required placeholder="Enter your new address..."><?php echo $rows['user_address']?></textarea>
+                                                        </div>
+                                                    </div> <!-- end col -->
+                                                </div> <!-- end row -->
+                                                    <label><u>*If you don't want to change password, let it be EMPTY*</u></label><br>
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label for="password">Old Password</label>
+                                                            <input type="password" name="password" parsley-trigger="change" placeholder="Enter current password" class="form-control" id="password">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label for="New_password">New Password</label>
+                                                            <input type="password" name="New_password" parsley-trigger="change" placeholder="Enter new password" class="form-control" id="New_password">
+                                                        </div>
+                                                    </div>
                                                 </div> <!-- end row -->
 
                                                 <div class="text-center">
