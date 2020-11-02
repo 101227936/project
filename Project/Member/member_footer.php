b273062f37f5152f78d284e32330d2ce523b4032
Jun0715 <62086447+Jun0715@users.noreply.github.com>
2020-11-02 15:08:11 +0800

    Design layout of member footer

--- Project/Member/member_footer.php
+++ Project/Member/member_footer.php
@@ -2,13 +2,12 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6">
-                2015 - <script>document.write(new Date().getFullYear())</script> &copy; UBold theme by <a href="">Coderthemes</a> 
+                <script>document.write(new Date().getFullYear())</script> &copy; <a href="" class="btn2">FoodEdge Gourmet. </a>All rights reserved.
             </div>
         <div class="col-md-6">
             <div class="text-md-right footer-links d-none d-sm-block">
-                <a href="javascript:void(0);">About Us</a>
-                <a href="javascript:void(0);">Help</a>
-                <a href="javascript:void(0);">Contact Us</a>
+                <a href="">About Us</a>
+                <a href="">Contact Us</a>
             </div>
         </div>
     </div>
