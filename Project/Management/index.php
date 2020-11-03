b4060d9dcbab7d1eed23ed0a20753c783d2b6b61
101227936 <101227936@students.swinburne.edu.my>
2020-11-04 07:59:50 +0800

    Delete index.php

--- Project/Management/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require '../Database/init.php';
-	$db->where("login_id>1")->where("email='fcmsmember@gmail.com'");
-	$users = $db->get('tbl_login');
-	print_r ("<pre>");
-	print_r($users);
-	print_r ("</pre>");
-?>
