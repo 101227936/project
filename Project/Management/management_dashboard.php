013b2df1d957c789851b9eb7b7fa872e1afe754e
elaine_0207 <elaine_0207@yahoo.com>
2020-11-04 16:29:09 +0800

    Update management_dashboard.php

--- Project/Management/management_dashboard.php
+++ Project/Management/management_dashboard.php
@@ -54,6 +54,189 @@
 			$db->join("tbl_product_detail","tbl_product_detail.product_id=tbl_product.product_id","INNER");
 			$products=$db->get("tbl_product");
 		?>
+		
+		<?php
+			$actual_sales_total = 0;
+			$sales_1 = 0;
+			$sales_2 = 0;
+			$sales_3 = 0;
+			$sales_4 = 0;
+			$sales_5 = 0;
+			$sales_6 = 0;
+			$sales_7 = 0;
+			$sales_8 = 0;
+			$sales_9 = 0;
+			$sales_10 = 0;
+			$sales_11 = 0;
+			$sales_12 = 0;
+			$cust_1 = 0;
+			$cust_2 = 0;
+			$cust_3 = 0;
+			$cust_4 = 0;
+			$cust_5 = 0;
+			$cust_6 = 0;
+			$cust_7 = 0;
+			$cust_8 = 0;
+			$cust_9 = 0;
+			$cust_10 = 0;
+			$cust_11 = 0;
+			$cust_12 = 0;
+			$today = new DateTime();
+			$today_Year = $today->format("Y");
+			foreach($members as $order)
+			{
+				$date = new DateTime($order['order_datetime']);
+				$orderDate = $date->format("Y-m");
+				$orderYear = $date->format("Y");
+				$orderMonth = $date->format("m");
+				if($orderYear == $today_Year && $order['payment_status']=="Confirmed")
+				{
+					if($orderMonth == "01")
+					{
+						$sales_1 += $order['amount_price'];
+						$cust_1++;
+					}else if($orderMonth == "02")
+					{
+						$sales_2 += $order['amount_price'];
+						$cust_2++;
+					}else if($orderMonth == "03")
+					{
+						$sales_3 += $order['amount_price'];
+						$cust_3++;
+					}else if($orderMonth == "04")
+					{
+						$sales_4 += $order['amount_price'];
+						$cust_4++;
+					}else if($orderMonth == "05")
+					{
+						$sales_5 += $order['amount_price'];
+						$cust_5++;
+					}else if($orderMonth == "06")
+					{
+						$sales_6 += $order['amount_price'];
+						$cust_6++;
+					}else if($orderMonth == "07")
+					{
+						$sales_7 += $order['amount_price'];
+						$cust_7++;
+					}else if($orderMonth == "08")
+					{
+						$sales_8 += $order['amount_price'];
+						$cust_8++;
+					}else if($orderMonth == "09")
+					{
+						$sales_9 += $order['amount_price'];
+						$cust_9++;
+					}else if($orderMonth == "10")
+					{
+						$sales_10 += $order['amount_price'];
+						$cust_10++;
+					}else if($orderMonth =="11")
+					{
+						$sales_11 += $order['amount_price'];
+						$cust_11++;
+					}else if($orderMonth == "12")
+					{
+						$sales_12 += $order['amount_price'];
+						$cust_12++;
+					}
+				}
+			}
+		?>
+		
+		<script>
+			window.onload = function () {
+
+				var chart = new CanvasJS.Chart("chartContainer", {
+					animationEnabled: true,
+					theme: "light2",
+					title: {
+						text: "Monthly Sales & Customer Data"
+					},
+					axisX: {
+						valueFormatString: "MMM"
+					},
+					axisY: {
+						prefix: "RM",
+						labelFormatter: addSymbols
+					},
+					toolTip: {
+						shared: true
+					},
+					legend: {
+						cursor: "pointer",
+						itemclick: toggleDataSeries
+					},
+					data: [
+					{
+						type: "line",
+						name: "Sales",
+						showInLegend: true,
+						xValueFormatString: "MMM YYYY",
+						yValueFormatString: "RM#,##0",
+						dataPoints: [
+							
+							{ x: new Date(<?=$today_Year?>, 0), y: <?=$sales_1?> },
+							{ x: new Date(<?=$today_Year?>, 1), y: <?=$sales_2?> },
+							{ x: new Date(<?=$today_Year?>, 2), y: <?=$sales_3?> },
+							{ x: new Date(<?=$today_Year?>, 3), y: <?=$sales_4?> },
+							{ x: new Date(<?=$today_Year?>, 4), y: <?=$sales_5?> },
+							{ x: new Date(<?=$today_Year?>, 5), y: <?=$sales_6?> },
+							{ x: new Date(<?=$today_Year?>, 6), y: <?=$sales_7?> },
+							{ x: new Date(<?=$today_Year?>, 7), y: <?=$sales_8?> },
+							{ x: new Date(<?=$today_Year?>, 8), y: <?=$sales_9?> },
+							{ x: new Date(<?=$today_Year?>, 9), y: <?=$sales_10?>},
+							{ x: new Date(<?=$today_Year?>, 10), y: <?=$sales_11?> },
+							{ x: new Date(<?=$today_Year?>, 11), y: <?=$sales_12?> }
+						]
+					},
+					{
+						type: "line",
+						name: "Number of Customer",
+						showInLegend: true,
+						xValueFormatString: "MMM YYYY",
+						yValueFormatString: "#,##0",
+						dataPoints: [
+							
+							{ x: new Date(<?=$today_Year?>, 0), y: <?=$cust_1?> },
+							{ x: new Date(<?=$today_Year?>, 1), y: <?=$cust_2?> },
+							{ x: new Date(<?=$today_Year?>, 2), y: <?=$cust_3?> },
+							{ x: new Date(<?=$today_Year?>, 3), y: <?=$cust_4?> },
+							{ x: new Date(<?=$today_Year?>, 4), y: <?=$cust_5?> },
+							{ x: new Date(<?=$today_Year?>, 5), y: <?=$cust_6?> },
+							{ x: new Date(<?=$today_Year?>, 6), y: <?=$cust_7?> },
+							{ x: new Date(<?=$today_Year?>, 7), y: <?=$cust_8?> },
+							{ x: new Date(<?=$today_Year?>, 8), y: <?=$cust_9?> },
+							{ x: new Date(<?=$today_Year?>, 9), y: <?=$cust_10?>},
+							{ x: new Date(<?=$today_Year?>, 10), y: <?=$cust_11?> },
+							{ x: new Date(<?=$today_Year?>, 11), y: <?=$cust_12?> }
+						]
+					}]
+				});
+				chart.render();
+
+				function addSymbols(e) {
+					var suffixes = ["", "K", "M", "B"];
+					var order = Math.max(Math.floor(Math.log(e.value) / Math.log(1000)), 0);
+
+					if(order > suffixes.length - 1)                	
+						order = suffixes.length - 1;
+
+					var suffix = suffixes[order];      
+					return CanvasJS.formatNumber(e.value / Math.pow(1000, order)) + suffix;
+				}
+
+				function toggleDataSeries(e) {
+					if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+						e.dataSeries.visible = false;
+					} else {
+						e.dataSeries.visible = true;
+					}
+					e.chart.render();
+				}
+
+			}
+		</script>
 
         <!-- Begin page -->
         <div id="wrapper">
@@ -105,10 +288,13 @@
 													$total = 0;
 													foreach($members as $member)
 													{
-														$total += (int)$member['amount_price'];
+														if($member['payment_status'] == "Confirmed")
+														{
+															$total += (int)$member['amount_price'];
+														}
 													}
 												?>
-                                                <h3 class="text-dark mt-1">$<span data-plugin="counterup"><?=$total?>.00</span></h3>
+                                                <h3 class="text-dark mt-1">RM<span data-plugin="counterup"><?=$total?>.00</span></h3>
                                                 <p class="text-muted mb-1 text-truncate">Total Revenue</p>
                                             </div>
                                         </div>
@@ -201,49 +387,53 @@
                         <div class="row">
                             <div class="col-xl-7">
                                 <div class="card-box pb-2" style="height: 600px">
-                                    <!--
-									<div class="float-right d-none d-md-inline-block">
-                                        <div class="btn-group mb-2">
-                                            <button type="button" class="btn btn-xs btn-light">Today</button>
-                                            <button type="button" class="btn btn-xs btn-light">Weekly</button>
-                                            <button type="button" class="btn btn-xs btn-secondary">Monthly</button>
-                                        </div>
-                                    </div>
-									-->
+                                    <?php
+										$pre_sales = 0;
+										$cur_sales = 0;
+										$today = new DateTime();
+										$todayM = $today->format("Y-m");
+										$prevM = $today->modify("-1 month")->format("Y-m");
+										foreach($members as $order)
+										{
+											$date = new DateTime($order['order_datetime']);
+											$orderDate = $date->format("Y-m");
+											if($orderDate == $todayM && $order['payment_status']=="Confirmed")
+											{
+												$cur_sales += $order['amount_price'];
+											}else if($orderDate == $prevM && $order['payment_status']=="Confirmed")
+											{
+												$pre_sales += $order['amount_price'];
+											}
+										}
+									?>
                                     <h4 class="header-title mb-3">Sales Analytics</h4>
 
                                     <div class="row text-center">
-                                        <div class="col-md-4">
-                                            <p class="text-muted mb-0 mt-3">Current Week</p>
+                                        <div class="col-md-7">
+                                            <p class="text-muted mb-0 mt-3">Previous Month</p>
                                             <h2 class="font-weight-normal mb-3">
-                                                <small class="mdi mdi-checkbox-blank-circle text-primary align-middle mr-1"></small>
-                                                <span>$58,254</span>
+                                                <small class="mdi mdi-checkbox-blank-circle text-secondary align-middle mr-1"></small>
+                                                <span>RM<?=$pre_sales?>.00</span>
                                             </h2>
                                         </div>
                                         <div class="col-md-4">
-                                            <p class="text-muted mb-0 mt-3">Previous Week</p>
+                                            <p class="text-muted mb-0 mt-3">Current Month</p>
                                             <h2 class="font-weight-normal mb-3">
-                                                <small class="mdi mdi-checkbox-blank-circle text-success align-middle mr-1"></small>
-                                                <span>$69,524</span>
-                                            </h2>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <p class="text-muted mb-0 mt-3">Targets</p>
-                                            <h2 class="font-weight-normal mb-3">
-                                                <small class="mdi mdi-checkbox-blank-circle text-success align-middle mr-1"></small>
-                                                <span>$95,025</span>
+                                                <small class="mdi mdi-checkbox-blank-circle text-info align-middle mr-1"></small>
+                                                <span>RM<?=$cur_sales?>.00</span>
                                             </h2>
                                         </div>
                                     </div>
-
-                                    <div id="revenue-chart" class="apex-charts mt-3" data-colors="#6658dd,#1abc9c"></div>
+									
+									<div id="chartContainer" style="width: 100%;"></div>
+									<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
                                 </div> <!-- end card-box -->
                             </div> <!-- end col-->
 
                             <div class="col-xl-5">
 								<div class = "row">
 									<div class="col-xl">
-										<div class="card-box" style="width: auto">
+										<div class="card-box">
 											<h4 class="header-title mb-0">Sales</h4>
 											<div class="widget-chart text-center" dir="ltr">
 												<h5 class="text-muted mt-1">Total sales made today</h5>
@@ -262,22 +452,7 @@
 														}
 													}
 												?>
-												<h2>$<?=$sales?>.00</h2>
-												
-												<div class="row mt-2">
-													<div class="col-4">
-														<p class="text-muted font-15 mb-1 text-truncate">Target</p>
-														<h4><i class="fe-arrow-down text-danger mr-1"></i>$7.8k</h4>
-													</div>
-													<div class="col-4">
-														<p class="text-muted font-15 mb-1 text-truncate">Last week</p>
-														<h4><i class="fe-arrow-up text-success mr-1"></i>$1.4k</h4>
-													</div>
-													<div class="col-4">
-														<p class="text-muted font-15 mb-1 text-truncate">Last Month</p>
-														<h4><i class="fe-arrow-down text-danger mr-1"></i>$15k</h4>
-													</div>
-												</div>
+												<h2>RM<?=$sales?>.00</h2>												
 											</div>
 										</div>
 									</div>
@@ -285,7 +460,7 @@
 								
 								<div class = "row">
 									<div class="col-xl">
-										<div class="card-box"  style="position: relative;height: 370px;overflow: auto;display: block;">
+										<div class="card-box"  style="position: relative;height: 435px;overflow: auto;display: block;">
 
 											<h4 class="header-title mb-0">Reviews</h4>
 
@@ -372,7 +547,7 @@
 																		echo $sDate;
 																	?>
 																</td>
-																<td>$ <?=($member['amount_price'])?>.00</td>
+																<td>RM <?=($member['amount_price'])?>.00</td>
 																<?php
 																	if($member['payment_status']=='Waiting for Refund')
 																	{
@@ -433,7 +608,7 @@
 																			<p><span><?=$product['product_name']?></span></p>
 																		</td>
 																		<td><?=$product['product_detail_size']?></td>
-																		<td>$ <?=$product['product_detail_price']?>.00</td>
+																		<td>RM <?=$product['product_detail_price']?>.00</td>
 																		<?php
 																			if(strtolower($product['product_detail_status'])=='not available')
 																			{
@@ -455,7 +630,7 @@
 																	?>
 																	<tr>
 																		<td><?=$product['product_detail_size']?></td>
-																		<td>$ <?=$product['product_detail_price']?>.00</td>
+																		<td>RM <?=$product['product_detail_price']?>.00</td>
 																		<?php
 																			if(strtolower($product['product_detail_status'])=='not available')
 																			{
