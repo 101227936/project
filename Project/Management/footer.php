e6a02470b11fb681c333c1579c8e32ab78ee9dae
Jun0715 <62086447+Jun0715@users.noreply.github.com>
2020-11-09 12:14:45 +0800

    Create layout and implement navigation bar for management team

--- Project/Management/footer.php
+++ Project/Management/footer.php
@@ -1,19 +1,14 @@
-
-<!-- Footer Start -->
 <footer class="footer">
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-md-6">
-				2015 - <script>document.write(new Date().getFullYear())</script> &copy; UBold theme by <a href="">Coderthemes</a> 
-			</div>
-			<div class="col-md-6">
-				<div class="text-md-right footer-links d-none d-sm-block">
-					<a href="javascript:void(0);">About Us</a>
-					<a href="javascript:void(0);">Help</a>
-					<a href="javascript:void(0);">Contact Us</a>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <script>document.write(new Date().getFullYear())</script> &copy; <a href="" class="btn2">FoodEdge Gourmet. </a>All rights reserved.
+            </div>
+        <div class="col-md-6">
+            <div class="text-md-right footer-links d-none d-sm-block">
+                <a href="">About Us</a>
+                <a href="">Contact Us</a>
+            </div>
+        </div>
+    </div>
 </footer>
-<!-- end Footer -->
