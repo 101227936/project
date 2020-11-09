ee16b9f61c48fd0a59861817b120f94b8ffb8b67
Jun0715 <62086447+Jun0715@users.noreply.github.com>
2020-11-09 14:41:13 +0800

    Update member_topbar.php

--- Project/Member/member_topbar.php
+++ Project/Member/member_topbar.php
@@ -1,10 +1,9 @@
 <?php
-	require '../Database/init.php';
-	//require "../encrypt.php";
+	require "../encrypt.php";
 	ob_start();
 	session_start();
 	
-	//$_SESSION['user_id']=1;
+	$_SESSION['user_id']=1;
 	
 	$db->join("tbl_user", "tbl_order.user_id=tbl_user.user_id", "LEFT");
 	$db->where("tbl_order.user_id",$_SESSION['user_id'],"=");
