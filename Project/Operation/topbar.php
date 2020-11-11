a7a1f8a4843188526d8443a2bcfc36d95f01d6ca
Jun0715 <62086447+Jun0715@users.noreply.github.com>
2020-11-11 17:33:52 +0800

    Update top bar

--- Project/Operation/topbar.php
+++ Project/Operation/topbar.php
@@ -57,29 +57,9 @@
 						</div>
 					</li>
 				</ul> 
-
-				<ul class="list-unstyled topnav-menu topnav-menu-left m-0">
-					<li>
-						<button class="button-menu-mobile waves-effect waves-light">
-							<i class="fe-menu"></i>
-						</button>
-					</li>
-
-					<li>
-						<!-- Mobile menu toggle (Horizontal Layout)-->
-						<a class="navbar-toggle nav-link" data-toggle="collapse" data-target="#topnav-menu-content">
-							<div class="lines">
-								<span></span>
-								<span></span>
-								<span></span>
-							</div>
-						</a>
-						<!-- End mobile menu toggle-->
-					</li>   
-				</ul>
 				
 				<div class="logo-box">
-                        <a href="" class="logo logo-light text-center">
+                        <a href="order_list.php" class="logo logo-light text-center">
                             <span class="logo-sm">
                                 <img src="../Image/foodedge.png" alt="" height="30">
                             </span>
