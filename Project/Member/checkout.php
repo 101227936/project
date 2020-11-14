7f5ad9c2e5719a91e8513d918c480c3657f40d4f
elaine_0207 <elaine_0207@yahoo.com>
2020-11-14 16:59:16 +0800

    make changes

--- Project/Member/checkout.php
+++ Project/Member/checkout.php
@@ -45,24 +45,6 @@
                     <!-- Start Content-->
                     <div class="container-fluid">
                         
-                        <!-- start page title -->
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="page-title-box">
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">UBold</a></li>
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Ecommerce</a></li>
-                                            <li class="breadcrumb-item active">Checkout</li>
-                                        </ol>
-                                    </div>
-                                    <h4 class="page-title">Checkout</h4>
-                                </div>
-                            </div>
-                        </div>     
-                        <!-- end page title --> 
-						
-
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="card">
--- Project/Member/order_detail.php
+++ Project/Member/order_detail.php
@@ -41,24 +41,6 @@
 
                     <!-- Start Content-->
                     <div class="container-fluid">
-                        
-                        <!-- start page title -->
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="page-title-box">
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">UBold</a></li>
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Ecommerce</a></li>
-                                            <li class="breadcrumb-item active">Order Detail</li>
-                                        </ol>
-                                    </div>
-                                    <h4 class="page-title">Order Detail</h4>
-                                </div>
-                            </div>
-                        </div>     
-                        <!-- end page title --> 
-						
 						<?php
 							if(empty($_GET['order_id']))header("Location: order_list.php");
 							else
