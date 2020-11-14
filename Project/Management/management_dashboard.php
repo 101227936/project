e8ae85f1bbe170302b75c75cf9175161fff78d0b
elaine_0207 <elaine_0207@yahoo.com>
2020-11-14 16:52:50 +0800

    Update management_dashboard.php

--- Project/Management/management_dashboard.php
+++ Project/Management/management_dashboard.php
@@ -163,23 +163,6 @@
 
                     <!-- Start Content-->
                     <div class="container-fluid">
-                        
-                        <!-- start page title -->
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="page-title-box">
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">UBold</a></li>
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">eCommerce</a></li>
-                                            <li class="breadcrumb-item active">Dashboard</li>
-                                        </ol>
-                                    </div>
-                                    <h4 class="page-title">Dashboard</h4>
-                                </div>
-                            </div>
-                        </div>     
-                        <!-- end page title --> 
 
                         <div class="row">
                             <div class="col-md-6 col-xl-3">
